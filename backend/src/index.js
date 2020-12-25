426900a7f52512210d9a9d0548cb77cd8da800d2
Robert <rgs2@icomp.ufam.edu.br>
2020-12-25 15:33:23 -0400

    feat: Criando a parte de update

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,6 +25,28 @@ app.post("/projects", (req, res) => {
   return res.json(projects);
 });
 
+app.put("/projects/:id", (req, res) =>{
+  const { id } = req.params;
+  const { title, owner } = req.body;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if(projectIndex < 0) {
+    return res.status(401).json({ error: "Project not found" });
+  }
+
+  const project = {
+    id, 
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return res.json(projects);
+
+});
+
 app.listen(3333,
   console.log("Rodando o Backend")  
 );
